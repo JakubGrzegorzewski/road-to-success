63aa8faae077ee37bce70e8f0098f05b145665c0
Jakub Grzegorzewski <kgrzegorzewski@hypnoticants.com>
2025-07-25 20:49:55 +0200

    feat: implement rank fetching and enhance rank status display with new styles

--- src/components/MyRanks/SelectRankComponent.vue
+++ src/components/MyRanks/SelectRankComponent.vue
@@ -9,6 +9,10 @@ const props = defineProps({
   rankImage: {
     type: String,
   },
+  rankStatus: {
+    type: String,
+    required: false,
+  }
 })
 
 const resolvedImageSrc = computed(() => {
@@ -17,12 +21,37 @@ const resolvedImageSrc = computed(() => {
   }
   return ''
 })
+
+function selectColor(status) {
+  let color = 'color: '
+  switch (status) {
+    case 'creation':
+      color += 'var(--primary-color-dark)';
+      return color
+    case 'pending':
+      color += 'var(--accent-warning)';
+      return color
+    case 'inProgress':
+      color += 'var(--accent-info)';
+      return color
+    case 'completed':
+      color += 'var(--accent-success)';
+      return color
+    case 'failed':
+      color += 'var(--accent-error)';
+      return color
+    default:
+      color += 'var(--primary-color-dark)';
+      return color;
+  }
+}
 </script>
 
 <template>
   <div class="rank-component">
     <img :src="resolvedImageSrc" class="rank-image" :alt="rankName">
     <h2 class="rank-name">{{ rankName }}</h2>
+    <h3 v-if="props.rankStatus" class="rank-status" :style="selectColor(props.rankStatus)"> {{ $t(`advancement.statusOptions.${props.rankStatus}`) }} </h3>
   </div>
 </template>
 
@@ -37,17 +66,23 @@ const resolvedImageSrc = computed(() => {
   font-size: 1.5em;
   text-align: center;
   font-family: "Museo", sans-serif;
+  margin: 20px 0 0 0;
 }
 .rank-component {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   padding: 20px;
   border-radius: 20px;
   box-shadow: 0 2px 8px var(--shadow-light);
   width: 200px;
   height: 250px;
-
+}
+.rank-status {
+  font-size: 1em;
+  text-align: center;
+  font-family: "Museo", sans-serif;
+  margin: 0;
 }
 </style>
--- src/views/AdvancementsView.vue
+++ src/views/AdvancementsView.vue
@@ -3,10 +3,16 @@ import SelectRankComponent from "@/components/MyRanks/SelectRankComponent.vue";
 import {ref, onMounted} from 'vue';
 
 const ranks = ref([]);
+const allRanks = ref([]);
 const error = ref(null);
 
 onMounted(() => {
-  fetch(`/api/rank/user/${123}`, {
+  fetchUserRanks(123);
+  fetchAllRanks();
+})
+
+async function fetchUserRanks(id) {
+  fetch(`/api/rank/user/${id}`, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
@@ -20,23 +26,48 @@ onMounted(() => {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         ranks.value = await response.json();
+        console.log(ranks.value);
       })
       .catch(err => {
         console.error('Error fetching ranks:', err);
         error.value = err.message;
         ranks.value = null;
       });
-})
+}
+
+async function fetchAllRanks() {
+  fetch("/api/ranks", {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+      .then(async response => {
+        if (response.status === 403) {
+          throw new Error('Access forbidden - check authentication');
+        }
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        allRanks.value = await response.json();
+      })
+      .catch(err => {
+        console.error('Error fetching all ranks:', err);
+        error.value = err.message;
+        allRanks.value = null;
+      });
+}
 
 </script>
 
 <template>
-  <div class="all-ranks">
+  <div class="all-ranks" v-if="allRanks && ranks">
     <SelectRankComponent
         v-for="rank in ranks"
         :key="rank.rank"
-        :rankName="rank.rank"
+        :rankName="allRanks.find(element => element.rank_short === rank.rank)?.rank_name_full || 'rank not found'"
         :rankImage="`@/assets/images/${rank.rank}.png`"
+        :rankStatus="rank.status"
         @click="$router.push(`/Advancement/${rank.id}`)"
     />
     <SelectRankComponent
