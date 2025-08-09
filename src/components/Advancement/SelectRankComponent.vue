df49993dad7e44b70e2aedfd652042453b148152
Jakub Grzegorzewski <kgrzegorzewski@hypnoticants.com>
2025-08-09 12:39:00 +0200

    fix: update status translation path in SelectRankComponent.vue

--- src/components/Advancement/SelectRankComponent.vue
+++ src/components/Advancement/SelectRankComponent.vue
@@ -54,38 +54,37 @@ function selectColor(status) {
   <div class="rank-component">
     <img :src="resolvedImageSrc" class="rank-image" :alt="rankName">
     <h2 class="rank-name">{{ rankName }}</h2>
-    <h3 v-if="props.rankStatus" class="rank-status" :style="selectColor(props.rankStatus.toLowerCase())"> {{ $t(`advancement.statusOptions.${props.rankStatus.toLowerCase()}`) }} </h3>
+    <h3 v-if="props.rankStatus" class="rank-status" :style="selectColor(props.rankStatus.toLowerCase())"> {{ $t(`statusOptions.${props.rankStatus.toLowerCase()}`) }} </h3>
   </div>
 </template>
 
 <style scoped>
-
-.rank-image {
-  max-width: 100px;
-  max-height: 100px;
-  object-fit: contain;
-}
-.rank-name {
-  font-size: 1.5em;
-  text-align: center;
-  font-family: "Museo", sans-serif;
-  margin: 20px 0 0 0;
-}
-.rank-component {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-  border-radius: 20px;
-  box-shadow: 0 2px 8px var(--shadow-light);
-  width: 200px;
-  height: 250px;
-}
-.rank-status {
-  font-size: 1em;
-  text-align: center;
-  font-family: "Museo", sans-serif;
-  margin: 0;
-}
+  .rank-image {
+    max-width: 100px;
+    max-height: 100px;
+    object-fit: contain;
+  }
+  .rank-name {
+    font-size: 1.5em;
+    text-align: center;
+    font-family: "Museo", sans-serif;
+    margin: 20px 0 0 0;
+  }
+  .rank-component {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 2px 8px var(--shadow-light);
+    width: 200px;
+    height: 250px;
+  }
+  .rank-status {
+    font-size: 1em;
+    text-align: center;
+    font-family: "Museo", sans-serif;
+    margin: 0;
+  }
 </style>
