5d08b39db89a8ec9f28ebb18bdcd6a7ac06a073d
Jakub Grzegorzewski <kgrzegorzewski@hypnoticants.com>
2025-08-09 12:38:16 +0200

    feat: update App.vue to improve layout structure and manage locale/user cookies

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <script setup>
-  import NavBarComponent from "@/components/UniversalComponents/NavBarComponent.vue";
+import NavBarComponent from "@/components/Website/NavBarComponent.vue";
+import Cookies from 'js-cookie';
+import { onMounted } from 'vue';
+import FooterComponent from "@/components/Website/FooterComponent.vue";
+
+onMounted(() => {
+  if (!Cookies.get('locale')) {
+    Cookies.set('locale', 'pl', { expires: 365 });
+  }
+  if (!Cookies.get('userId')) {
+    Cookies.set('userId', '1', { expires: 1 });
+  }
+})
 </script>
 
 <template>
-  <nav-bar-component></nav-bar-component>
-  <router-view />
-  <footer>
-    <p>&copy; 2025 Jakub Grzegorzewski</p>
-  </footer>
+  <div class="app-container">
+    <nav-bar-component></nav-bar-component>
+    <main class="main-content">
+      <router-view />
+    </main>
+    <footer-component/>
+  </div>
 </template>
 
 <style scoped>
-  @import url('assets/css/default.css');
-  @import url('assets/css/custom_components.css');
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+}
 </style>
 
 <style>
-  @import url('assets/css/default.css');
-  @import url('assets/css/custom_components.css');
+@import url('assets/css/default.css');
+@import url('assets/css/custom_components.css');
+
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
