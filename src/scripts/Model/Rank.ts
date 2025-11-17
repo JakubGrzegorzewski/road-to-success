c1661c5413185c6e23fc3a7593fe8a2124e8ef08
Jakub Grzegorzewski <kgrzegorzewski@hypnoticants.com>
2025-11-17 17:51:47 +0100

    feat: refactor Rank and Requirement classes to use fetchGET for data retrieval

--- src/scripts/Model/Rank.ts
+++ src/scripts/Model/Rank.ts
@@ -1,4 +1,4 @@
-let ranks : RankDTO[] = []
+import {fetchGET} from "@/scripts/helperFunctions.js";
 
 export interface RankDTO {
     id: number;
@@ -13,36 +13,15 @@ export interface RankDTO {
 
 export class Rank {
     static getAll(): Promise<RankDTO[]> {
-        return Promise.resolve(ranks);
+        return fetchGET("/zhp/advancements.json");
     }
 
-    static getById(id: number): Promise<RankDTO> {
-        return Promise.resolve(<RankDTO>ranks.find(rank => rank.id === id));
-    }
-
-    static add(rank: RankDTO): Promise<RankDTO> {
-        const exists = ranks.some(r => r.id === rank.id);
-        if (!exists) {
-            ranks.push(rank);
-        }
-        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
-        return Promise.resolve(rank);
-    }
-
-    static update(rank: RankDTO): Promise<RankDTO | undefined> {
-        const index = ranks.findIndex(u => u.id === rank.id)
-        if (index !== -1) {
-            ranks[index] = rank;
-            window.localStorage.setItem("Ranks", JSON.stringify(ranks));
-            return Promise.resolve(rank);
-        }
-        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
-        return Promise.resolve(undefined);
-    }
-
-    static deleteObject(id: number): void {
-        ranks = ranks.filter(rank => rank.id !== id)
-        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
+    static async getById(id: number): Promise<RankDTO> {
+        return fetchGET("/zhp/advancements.json").then((ranks: RankDTO[]) => {
+            const rank = ranks.find(rank => rank.id === id);
+            if (!rank) throw new Error(`Rank with id ${id} not found`);
+            return rank;
+        });
     }
 }
 
--- src/scripts/Model/Requirement.ts
+++ src/scripts/Model/Requirement.ts
@@ -1,5 +1,4 @@
-import { Rank } from "./Rank.js";
-let requirements : RequirementDTO[] = []
+import {fetchGET} from "@/scripts/helperFunctions.js";
 
 export interface RequirementDTO {
     id: number;
@@ -11,45 +10,15 @@ export interface RequirementDTO {
 
 export class Requirement {
     static getAll(): Promise<RequirementDTO[]> {
-        return Promise.resolve(requirements);
+        return fetchGET("/zhp/requirements.json");
     }
 
-    static getById(id: number): Promise<RequirementDTO> {
-        return Promise.resolve(<RequirementDTO>requirements.find(requirement => requirement.id === id));
-    }
-
-    static add(requirement: RequirementDTO, rankId : number): Promise<RequirementDTO> {
-        const exists = requirements.some(r => r.id === requirement.id);
-        if (!exists) {
-            requirements.push(requirement);
-            Rank.getById(rankId).then(rank => {
-                if (!rank.requirementIds.includes(requirement.id)) {
-                    rank.requirementIds.push(requirement.id);
-                    Rank.update(rank);
-                }
-            });
-        }
-        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
-        return Promise.resolve(requirement);
-    }
-
-    static update(requirement: RequirementDTO): Promise<RequirementDTO | undefined> {
-        const index = requirements.findIndex(u => u.id === requirement.id)
-        if (index !== -1) {
-            requirements[index] = requirement;
-            return Promise.resolve(requirement);
-        }
-        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
-        return Promise.resolve(undefined);
-    }
-
-    static deleteObject(id: number, rankId : number): void {
-        requirements = requirements.filter(requirement => requirement.id !== id)
-        Rank.getById(rankId).then(rank => {
-            rank.requirementIds = rank.requirementIds.filter(requirementId => requirementId !== id);
-            Rank.update(rank);
-        })
-        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
+    static async getById(id: number): Promise<RequirementDTO> {
+        return fetchGET("/zhp/requirements.json").then((ranks: RequirementDTO[]) => {
+            const rank = ranks.find(rank => rank.id === id);
+            if (!rank) throw new Error(`Rank with id ${id} not found`);
+            return rank;
+        });
     }
 }
 
