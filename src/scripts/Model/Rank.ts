f3a4bb2def1b5b263b4a454855f4f9c812fbe34b
Jakub Grzegorzewski <82871063+JakubGrzegorzewski@users.noreply.github.com>
2025-11-29 22:05:58 +0100

    Adding ability to have local Ranks

--- src/scripts/Model/Rank.ts
+++ src/scripts/Model/Rank.ts
@@ -13,15 +13,47 @@ export interface RankDTO {
 
 export class Rank {
     static getAll(): Promise<RankDTO[]> {
-        return new Promise(resolve => resolve(rank));
+        let localData = JSON.parse(window.localStorage.getItem("Ranks") || "[]") as RankDTO[];
+        let staticData = rank as RankDTO[];
+        return new Promise(resolve => resolve(
+            [...staticData, ...localData.filter((localRank: RankDTO) => !staticData.some(staticRank => staticRank.id === localRank.id))]
+        ));
     }
 
     static async getById(id: number): Promise<RankDTO> {
         return this.getAll().then((ranks: RankDTO[]) => {
-            const rank = ranks.find(rank => rank.id === id);
+            let rank = ranks.find(rank => rank.id === id);
             if (!rank) throw new Error(`Rank with id ${id} not found`);
             return rank;
         });
+    }
+
+    static add(rank: RankDTO): Promise<RankDTO> {
+        let ranks = JSON.parse(window.localStorage.getItem("Ranks") || "[]") as RankDTO[];
+        const exists = ranks.some(r => r.id === rank.id);
+        if (!exists) {
+            ranks.push(rank);
+        }
+        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
+        return Promise.resolve(rank);
+    }
+
+    static update(rank: RankDTO): Promise<RankDTO | undefined> {
+        let ranks = JSON.parse(window.localStorage.getItem("Ranks") || "[]") as RankDTO[];
+        const index = ranks.findIndex(u => u.id === rank.id)
+        if (index !== -1) {
+            ranks[index] = rank;
+            window.localStorage.setItem("Ranks", JSON.stringify(ranks));
+            return Promise.resolve(rank);
+        }
+        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
+        return Promise.resolve(undefined);
+    }
+
+    static deleteObject(id: number): void {
+        let ranks = JSON.parse(window.localStorage.getItem("Ranks") || "[]") as RankDTO[];
+        ranks = ranks.filter(rank => rank.id !== id)
+        window.localStorage.setItem("Ranks", JSON.stringify(ranks));
     }
 }
 
--- src/scripts/Model/Requirement.ts
+++ src/scripts/Model/Requirement.ts
@@ -10,7 +10,12 @@ export interface RequirementDTO {
 
 export class Requirement {
     static getAll(): Promise<RequirementDTO[]> {
-        return new Promise(resolve => resolve(requirements));
+        let localData = JSON.parse(window.localStorage.getItem("Requirements") || "[]") as RequirementDTO[];
+        let staticData = requirements as RequirementDTO[];
+        return new Promise(resolve => resolve(
+            [...staticData, ...localData.filter((localRequirement: RequirementDTO) => !staticData.some(staticRequirement => staticRequirement.id === localRequirement.id))
+            ]
+        ));
     }
 
     static async getById(id: number): Promise<RequirementDTO> {
@@ -19,6 +24,34 @@ export class Requirement {
             if (!rank) throw new Error(`Rank with id ${id} not found`);
             return rank;
         });
+    }
+
+    static add(requirement: RequirementDTO): Promise<RequirementDTO> {
+        let requirements = JSON.parse(window.localStorage.getItem("Requirements") || "[]") as RequirementDTO[];
+        const exists = requirements.some(r => r.id === requirement.id);
+        if (!exists) {
+            requirements.push(requirement);
+        }
+        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
+        return Promise.resolve(requirement);
+    }
+
+    static update(requirement: RequirementDTO): Promise<RequirementDTO | undefined> {
+        let requirements = JSON.parse(window.localStorage.getItem("Requirements") || "[]") as RequirementDTO[];
+        const index = requirements.findIndex(u => u.id === requirement.id)
+        if (index !== -1) {
+            requirements[index] = requirement;
+            window.localStorage.setItem("Requirements", JSON.stringify(requirements));
+            return Promise.resolve(requirement);
+        }
+        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
+        return Promise.resolve(undefined);
+    }
+
+    static deleteObject(id: number): void {
+        let requirements = JSON.parse(window.localStorage.getItem("Requirements") || "[]") as RequirementDTO[];
+        requirements = requirements.filter(requirement => requirement.id !== id)
+        window.localStorage.setItem("Requirements", JSON.stringify(requirements));
     }
 }
 
